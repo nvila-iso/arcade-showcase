f541d10bd33416ee3179ba870b8e8ab503169e09
nvila-iso <nvila88@proton.me>
2025-10-30 23:37:48 -0400

    style changes for homepage and games page

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,25 +1,65 @@
 import { Link } from "react-router";
+import arcadeGames from "../data/arcade_games";
 
 const Games = () => {
+  const platforms = ["ARCADE", "PINBALL"];
+
+  const genres = [
+    "Fighting",
+    "Rhythm",
+    "Light Gun",
+    "STG (Shmups)",
+    "Beat-Em-Ups",
+    "Puzzle",
+    "Platformer",
+    "Other",
+  ];
+
   return (
     <>
-      <div className="max-w-5xl mx-auto flex flex-col md:flex-row overflow-hidden flex h-screen justify-center items-center">
-        <div className="flex justify-center items-center">
+      <div className="max-w-6xl flex flex-col overflow-hidden flex h-screen mx-auto justify-center px-5">
+        <div className="flex justify-center items-center gap-3">
           <Link to="">
             <p className="text-white font-bold text-2xl">HOME</p>
           </Link>
-          <p className="text-5xl text-orange-500 font-bold text-shadow-[3px_3px_0px_rgb(0_0_0_/_1)] bg-yellow-400 rounded-2xl border-2 border-blue-600 py-2 px-3 shadow-[0_5px_0px_rgba(0,0,0,1)]">GAMES</p>
+          <p className="text-5xl text-orange-500 font-bold text-shadow-[3px_3px_0px_rgb(0_0_0_/_1)] bg-yellow-400 rounded-2xl border-2 border-blue-600 py-2 px-3 shadow-[0_5px_0px_rgba(0,0,0,1)]">
+            GAMES
+          </p>
           <Link to="">
             <p className="text-white font-bold text-2xl">EVENTS</p>
           </Link>
         </div>
-        <div className="grid grid-col-2">
-          <div id="menu">
-            <div>
-              <p>FILTER</p>
-            </div>
+        <div className="mt-10 grid grid-cols-1">
+          <input
+            type="text"
+            className="w-full h-12 rounded-full bg-white border-2 px-5"
+            placeholder="SEARCH . . ."
+          />
+          <div className="mt-5 grid grid-cols-2 justify-center gap-5">
+            <select className="border border-black px-2 h-10 bg-white rounded">
+              <option value="">ALL PLATFORMS</option>
+              {platforms.map((p, i) => (
+                <option key={i} value={p}>
+                  {p}
+                </option>
+              ))}
+            </select>
+            <select className="border border-black px-2 h-10 bg-white rounded">
+              <option value="">ALL GENRES</option>
+              {genres.map((g, i) => (
+                <option key={i} value={g}>{g}</option>
+              ))}
+            </select>
           </div>
-          <div id="games"></div>
+          {/* <div className="grid grid-cols-2 gap-2 h-[20%] overflow-auto">
+              {arcadeGames.map((arcade, i) => (
+                <>
+                <div>
+                  <p>{arcade.name}</p>
+                </div>
+                </>
+              ))}
+          </div> */}
         </div>
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,46 +4,46 @@ import character from "../assets/character-home.png";
 
 const Home = () => {
   return (
-    <div className="max-w-5xl flex flex-col md:flex-row overflow-hidden flex h-screen">
+    <div className="max-w-6xl flex flex-col md:flex-row overflow-hidden flex h-screen">
       <div className="md:relative md:top-50 md:left-10">
         <Link to="">
-          <p className="text-8xl text-center md:text-left font-bold text-red-600 mb-5 text-shadow-[-3px_3px_0px_rgb(0_0_0_/_1)]">
-            ARCADE
+          <p className="font-archivo text-6xl text-center md:text-left text-[#E4494F] mb-5 text-shadow-[-3px_3px_0px_rgb(0_0_0_/_1)] mt-5 title">
+            ARCADIA MANOR
           </p>
         </Link>
-        <div className="flex font-bold text-center justify-center items-center md:justify-start">
+        <div className="flex font-black text-center justify-center items-center md:justify-start">
           <Link to="/home">
-            <p className="rounded-l bg-yellow-400 text-blue-800 px-1 py-2 w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
+            <p className="rounded-l bg-yellow-400 text-blue-800 px-1 py-2 w-24 md:w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
               HOME
             </p>
           </Link>
           <Link to="/games">
-            <p className="bg-yellow-400 text-blue-800 px-1 py-2 w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
+            <p className="bg-yellow-400 text-blue-800 px-1 py-2 w-24 md:w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
               GAMES
             </p>
           </Link>
           <Link to="">
-            <p className="bg-yellow-400 text-blue-800 px-1 py-2 w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
+            <p className="bg-yellow-400 text-blue-800 px-1 py-2 w-24 md:w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
               ABOUT
             </p>
           </Link>
           <Link to="">
-            <p className="rounded-r bg-yellow-400 text-blue-800 px-1 py-2 w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
+            <p className="rounded-r bg-yellow-400 text-blue-800 px-1 py-2 w-24 md:w-30 border-2 border-blue-800 hover:bg-yellow-500 transition">
               CONTACT
             </p>
           </Link>
         </div>
-        <p className="ml-1 mr-1 mt-10 p-10 text-5xl text-center border-2 border-blue-800 bg-purple-300 text-blue-800 font-bold rounded md:ml-0 md:mr-0">
+        <p className="mt-10 p-8 text-4xl text-center border-2 border-blue-800 bg-purple-300 text-blue-800 font-black rounded w-[90%] mx-auto">
           WELCOME TO THE ARCADE!
         </p>
-        <div className="mt-10 flex gap-5 justify-center items-center">
+        <div className="relative top-65 flex w-[90%] justify-between items-center mx-auto">
           <Link to="">
-            <p className="rounded bg-yellow-400 text-blue-800 border-2 border-blue-800 px-1 py-2 w-45 text-center font-bold text-xl hover:bg-yellow-500 transition">
+            <p className="rounded w-40 bg-yellow-400 text-blue-800 border-2 border-blue-800 px-1 py-2 md:w-45 text-center font-bold text-xl hover:bg-yellow-500 transition ">
               PLAY NOW
             </p>
           </Link>
           <Link to="">
-            <p className="rounded bg-yellow-400 text-blue-800 border-2 border-blue-800 px-1 py-2 w-45 text-center font-bold text-xl hover:bg-yellow-500 transition">
+            <p className="rounded w-40 bg-yellow-400 text-blue-800 border-2 border-blue-800 px-1 py-2 md:w-45 text-center font-bold text-xl hover:bg-yellow-500 transition">
               LEARN MORE
             </p>
           </Link>
@@ -53,7 +53,7 @@ const Home = () => {
       <img
         src={character}
         alt=""
-        className="md:relative md:top-25 md:right-0 md:max-h-[90%]"
+        className="relative bottom-15 max-w-[80%] mx-auto md:relative md:top-25 md:right-0 md:max-h-[90%] -z-10"
       />
     </div>
   );
